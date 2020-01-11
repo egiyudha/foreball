f7d9b56e7503d1b1d20808ee17e3e3d23e420001
eynirvana <eynirvana@gmail.com>
2020-01-11 10:41:27 +0700

    set Error

--- app/src/main/java/com/yudha/foreball/service/dto/LeagueResponse.kt
+++ app/src/main/java/com/yudha/foreball/service/dto/LeagueResponse.kt
@@ -3,7 +3,10 @@ package com.yudha.foreball.service.dto
 import com.yudha.foreball.model.League
 
 data class LeagueResponse(val list: List<Leagues>)
-
+/*
+*
+* Problem here
+* */
 data class Leagues(val league: String) {
     fun toLeague(): League {
         return League(0, league)
